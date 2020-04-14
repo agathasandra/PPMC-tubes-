18da6b515b564e9d33a53fe38691bf5127637cd9
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-15 01:00:28 +0700

    Create display.c

---
+++ display.c
@@ -0,0 +1,10 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 9 – TUGAS BESAR
+* Kelompok : 5 (Rombongan E)
+* Hari dan Tanggal : 
+* Asisten (NIM) : Wuri Utami (13217024)
+* Nama File : display.c
+* Deskripsi : Fungsi untuk menampilkan array ke layar dengan jeda 250ms
+*/
+
+#include<stdio.h>
