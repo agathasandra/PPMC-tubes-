aa7baa2323bb617cc89b8ac4b2a7c3cc67b8c6e9
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-17 01:41:46 +0700

    Update game-of-life.c

--- game-of-life.c
+++ game-of-life.c
@@ -17,6 +17,7 @@ void display(char **array_main,int baris,int kolom);
 char** readFile(int * max_row, int * max_col);
 
 int main(){
+    //Deklarasi Variabel
     FILE *fi;
     int m,n,i,j,k;
     int baris = 0;
@@ -40,10 +41,11 @@ int main(){
     printf("4. Jika sel memiliki 4 tetangga yang hidup, maka sel tersebut akan mati pada iterasi selanjutnya /overpopulation.\n\n");
     printf("Mari kita mulai permainannya!\n");
 
-    array_main = readFile(&baris, &kolom);
+    array_main = readFile(&baris, &kolom); //menghubungkan ke fungsi readFile
     if (array_main != NULL){
         display(array_main, baris, kolom);
     }
+    //Loop untuk pilihan user mengenai apa yang harus dilakukan terhadap seed selanjutnya
     while (pilihan !=3 && baris!=99999 && kolom!=99999){
         printf("\n\nBerikut adalah beberapa aksi yang dapat dipilih: \n");
         printf("1. Tick\n");
@@ -51,26 +53,31 @@ int main(){
         printf("3. Quit\n\n");
         printf("Pilihan nomor aksi yang diinginkan: ");
         scanf("%d",&pilihan);
-
+        //Kondisi bila user ingin melakukan tick
         if (pilihan == 1){
             tick(array_main, baris, kolom);
             display(array_main, baris, kolom);
         }
+	//Kondisi bila user ingin melakukan perintah animate
         else if (pilihan == 2){
             printf("Masukkan jumlah iterasi: ");
             scanf("%d", &iterasi);
+	    //Animate akan dilakukan berdasarkan jumlah iterasi yang diinginkan oleh user
             for(i=0; i < iterasi; i++){
                 tick(array_main, baris, kolom);
                 display(array_main, baris, kolom);
             }
         }
+	//Kondisi bila user memilih perintah quit
+        //User akan diberikan pilihan untuk benar-benar quit atau untuk memasukkan seed baru
         else if (pilihan == 3){
-            loop = 1;
+            loop = 1; //agar loop dapat terus terjadi hingga input benar
             while(loop == 1){
                 printf("Apakah Anda mau memasukkan file seed baru?(Y/N)\n");
                 scanf(" %c", &baru);
                 if (baru == 'Y' || baru == 'y'){
-                    free(array_main);
+                    //Jika memilih yes, user akan diarahkan kembali ke menu utama pemilihan seed
+	  	    free(array_main);
                     pilihan = 99;
                     loop = 0;
                     array_main = readFile(&baris, &kolom);
@@ -78,16 +85,19 @@ int main(){
                         display(array_main, baris, kolom);
                     }
                 }
+		//User memilih untuk keluar
                 else if (baru == 'N' || baru == 'n'){
                     printf("\nSelamat tinggal dan terima kasih telah bermain Game of Life\n");
-                    loop = 0;
+                    loop = 0; //tidak akan terjadi loop lagi
                 }
                 else{
+		    //kondisi ketika input di luar Y/y dan N/n
                     printf("\nInput tidak Valid.\n");
                 }
             }
         }
         else{
+	    //Kondisi ketika user memasukkan input di luar 1(tick), 2(animate, dan 3(quit)
             printf("\nPilihan tidak Valid.\n");
         }
     }
@@ -95,24 +105,28 @@ int main(){
 }
 
 void display(char **array_main, int baris, int kolom){
+    //Deklarasi Variabel
     int ms = 250, i, j;
     clock_t start = clock();
-    while(clock() < start + ms);
+    while(clock() < start + ms); //Kondisi ketika waktu awal lebih kecil daripada waktu awal ditambah jeda
+    //Program akan melakukan jeda selama 250ms untuk menjalankan animasi
     system("cls");
     for(i=0; i<baris; i++){
         for(j=0; j<kolom; j++){
-            printf("%c", array_main[i][j]);
+            printf("%c", array_main[i][j]); //print array hasil setiap iterasi
         }
         printf("\n");
     }
 }
 
 void tick(char **array_main, int m, int n){
+    //Deklarasi Variabel
     int i, j, k, atas, bawah, kanan, kiri;
     int kounter = 0;
     int indeks = 0;
     char temp2[8];
     char *temp=(char *)malloc(m*n*sizeof(char));
+    //Cek kondisi tetangga dari sel
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
             if (i == 0){
@@ -139,6 +153,7 @@ void tick(char **array_main, int m, int n){
                 kiri = j-1;
                 kanan = j+1;
             }
+            //Kondisi tetangga sel
             temp2[0] = array_main[atas][kiri];
             temp2[1] = array_main[atas][j];
             temp2[2] = array_main[atas][kanan];
@@ -152,6 +167,7 @@ void tick(char **array_main, int m, int n){
                     kounter = kounter+1;
                 }
             }
+            //Cetak kondisi sel (hidup/mati) sesuai dengan aturan permainan
             if (array_main[i][j] == 'X'){
                 if (kounter == 2 || kounter == 3){
                     temp[indeks] = 'X';
@@ -176,13 +192,14 @@ void tick(char **array_main, int m, int n){
     indeks = 0;
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
-            array_main[i][j] = temp[indeks];
+            array_main[i][j] = temp[indeks]; //kondisi setelah iterasi
             indeks = indeks+1;
         }
     }
 }
 
 char** readFile(int * max_row, int * max_col) {
+	//Deklarasi Variabel
 	char read,nama_file[50];
 	int row = 0;
 	int column = 0;
@@ -203,19 +220,19 @@ char** readFile(int * max_row, int * max_col) {
 
     input_nama_file:
     printf("\nMasukkan nama file seed: ");
-    scanf("%s",nama_file);
+    scanf("%s", nama_file);
 
-    if (strcmp(nama_file,"q")==0){
+    if (strcmp(nama_file,"q") == 0){ //bila user memasukkan input q, maka program akan berhenti dan user keluar
         printf("\nTerima kasih\n");
-        *max_row=99999;
-        *max_col=99999;
+        *max_row = 99999;
+        *max_col = 99999;
         return(NULL);
     }
 
-    file = fopen(nama_file,"r");
+    file = fopen(nama_file, "r");
     if (file==NULL){
-        printf("File cant open\n");
-        goto input_nama_file;
+        printf("File cant open\n"); //Jika user memasukkan nama file yang tidak ada
+        goto input_nama_file; //user akan kembali memasukkan input file hingga input benar
     }
 
 	read = fgetc(file);
@@ -229,7 +246,7 @@ char** readFile(int * max_row, int * max_col) {
 		column = (column * 10) + (int)read - (int)'0';
 		read = fgetc(file);
 	}
-
+    //Mengubah atau memasukkan input ke dalam bentuk array 2x2
 	char **matrix = (char **)malloc(row * sizeof(char *));
 
 	for(int i = 0; i < row; i++) {
@@ -250,5 +267,3 @@ char** readFile(int * max_row, int * max_col) {
     fclose(file);
     return matrix;
 }
-
-
