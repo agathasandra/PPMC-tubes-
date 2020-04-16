4df6f1c156cd2758df7a638b6df9061542847f04
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-16 23:47:11 +0700

    Update copy_matrix.c

--- copy_matrix.c
+++ copy_matrix.c
@@ -3,7 +3,7 @@
 * Kelompok : 5 (Rombongan E)
 * Hari dan Tanggal : Kamis, 16 April 2020
 * Asisten (NIM) : Wuri Utami (13217024)
-* Nama File : game-of-life.c
+* Nama File : copy_matrix.c
 * Deskripsi : Fungsi untuk melakukan copy terhadap matrix yang telah terbentuk
 */
 
