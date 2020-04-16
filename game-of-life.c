1c27fe28465a63cdb3a55c8521a84c6e98d2dc70
Agatha S <63316442+agathasandra@users.noreply.github.com>
2020-04-17 01:00:44 +0700

    Update game-of-life.c

--- game-of-life.c
+++ game-of-life.c
@@ -1,10 +1,10 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
 *  MODUL 9 – TUGAS BESAR
-*  Kelompok : 5 (Rombongan E)
-*  Hari dan Tanggal : Kamis, 16 April 2020
-*  Asisten (NIM) : Wuri Utami (13217024)
-*  Nama File : game-of-life.c
-*  Deskripsi : Program utama yang mensimulasikan game of life
+*  Kelompok 		: 5 (Rombongan E)
+*  Hari dan Tanggal 	: Kamis, 16 April 2020
+*  Asisten (NIM) 	: Wuri Utami (13217024)
+*  Nama File 		: game-of-life.c
+*  Deskripsi 		: Program utama yang mensimulasikan game of life
 */
 #include<stdio.h>
 #include<stdlib.h>
